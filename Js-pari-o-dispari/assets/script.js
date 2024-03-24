5e74e83c9490b0ee3f9e4c2c616b26767652b63e
Gianpaolocastellaneta <160965826+Gianpaolocastellaneta@users.noreply.github.com>
2024-03-24 11:01:52 +0100

    Add files via upload

---
+++ Js-pari-o-dispari/assets/script.js
@@ -0,0 +1,8 @@
+const numero = prompt("Inserisci un numero")
+
+//controllo se pari o dispari
+if (numero % 2 === 0){
+    alert("Il numero inserito è pari!")
+}else{
+    alert("il numero inserito è dispari!")
+}
